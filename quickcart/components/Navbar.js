f2f123e1c66e06673558e9f38fe9aeac2822bedb
pkcoder471 <pklid471@gmail.com>
2024-02-16 20:03:32 +0530

    navbar start

--- quickcart/components/Navbar.js
+++ quickcart/components/Navbar.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
   return (
-    <div className='nav'>
+    <div className='nav flex'>
+      <div className="hamburger-icon">
+      <GiHamburgerMenu />
+      </div>
+      <div className="logo">
+      <h1>QuickCart</h1>
+      </div>
+      <div className="list absolute mt-5">
+        <ul>
+          <li>Tshirts</li>
+          <li>Hoodies</li>
+          <li>Stickers</li>
+          <li>Mugs</li>
+        </ul>
+      </div>
+      <div className="user">
+        <div className="Cart">Cart</div>
+        <div className="profile">Profile</div>
+      </div>
     </div>
   )
 }
