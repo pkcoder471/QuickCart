76fb6540c54e120f438a832f76e23e762f6fc561
pkcoder471 <pklid471@gmail.com>
2024-02-17 19:43:53 +0530

    hoodies page

--- quickcart/pages/hoodies.js
+++ quickcart/pages/hoodies.js
@@ -1,10 +1,95 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const hoodies = () => {
   return (
-    <div>
-      this is hoodies page
-    </div>
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-16 mx-auto">
+        <div className="flex flex-wrap -m-4">
+          <Link href={'/product/hoodie'}><div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-sm ">
+            <a className="  relative h-80 m-auto rounded overflow-hidden">
+              <Image alt="ecommerce" className="h-80 m-auto block" src="https://m.media-amazon.com/images/I/71TzNW2FArL._SY741_.jpg"/>
+            </a>
+            <div className="mt-4">
+              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+              <h2 className="text-gray-900 title-font text-lg font-medium">Hoodies</h2>
+              <p className="mt-1">₹499</p>
+            </div>
+          </div></Link>
+          <Link href={'/product/hoodie'}><div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-sm ">
+            <a className="  relative h-80 m-auto rounded overflow-hidden">
+              <Image alt="ecommerce" className="h-80 m-auto block  " src="https://m.media-amazon.com/images/I/71TzNW2FArL._SY741_.jpg"/>
+            </a>
+            <div className="mt-4">
+              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+              <h2 className="text-gray-900 title-font text-lg font-medium">Hoodies</h2>
+              <p className="mt-1">₹499</p>
+            </div>
+          </div></Link>
+          <Link href={'/product/hoodie'}><div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-sm ">
+            <a className="  relative h-80 m-auto rounded overflow-hidden">
+              <Image alt="ecommerce" className="h-80 m-auto block  " src="https://m.media-amazon.com/images/I/71TzNW2FArL._SY741_.jpg"/>
+            </a>
+            <div className="mt-4">
+              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+              <h2 className="text-gray-900 title-font text-lg font-medium">Hoodies</h2>
+              <p className="mt-1">₹499</p>
+            </div>
+          </div></Link>
+          <Link href={'/product/hoodie'}><div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-sm ">
+            <a className="  relative h-80 rounded overflow-hidden">
+              <Image alt="ecommerce" className="h-80 m-auto block  " src="https://m.media-amazon.com/images/I/71TzNW2FArL._SY741_.jpg"/>
+            </a>
+            <div className="mt-4">
+              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+              <h2 className="text-gray-900 title-font text-lg font-medium">Hoodies</h2>
+              <p className="mt-1">₹499</p>
+            </div>
+          </div></Link>
+          <Link href={'/product/hoodie'}><div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-sm ">
+            <a className="  relative h-80 m-auto rounded overflow-hidden">
+              <Image alt="ecommerce" className="h-80 m-auto block  " src="https://m.media-amazon.com/images/I/71TzNW2FArL._SY741_.jpg"/>
+            </a>
+            <div className="mt-4">
+              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+              <h2 className="text-gray-900 title-font text-lg font-medium">Hoodies</h2>
+              <p className="mt-1">₹499</p>
+            </div>
+          </div></Link>
+          <Link href={'/product/hoodie'}><div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-sm ">
+            <a className="  relative h-80 m-auto rounded overflow-hidden">
+              <Image alt="ecommerce" className="h-80 m-auto block  " src="https://m.media-amazon.com/images/I/71TzNW2FArL._SY741_.jpg"/>
+            </a>
+            <div className="mt-4">
+              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+              <h2 className="text-gray-900 title-font text-lg font-medium">Hoodies</h2>
+              <p className="mt-1">₹499</p>
+            </div>
+          </div></Link>
+          <Link href={'/product/hoodie'}><div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-sm ">
+            <a className="  relative h-80 m-auto rounded overflow-hidden">
+              <Image alt="ecommerce" className="h-80 m-auto block  " src="https://m.media-amazon.com/images/I/71TzNW2FArL._SY741_.jpg"/>
+            </a>
+            <div className="mt-4">
+              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+              <h2 className="text-gray-900 title-font text-lg font-medium">Hoodies</h2>
+              <p className="mt-1">₹499</p>
+            </div>
+          </div></Link>
+          <Link href={'/product/hoodie'}><div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-sm ">
+            <a className="  relative h-80 m-auto rounded overflow-hidden">
+              <Image alt="ecommerce" className="h-80 m-auto block  " src="https://m.media-amazon.com/images/I/71TzNW2FArL._SY741_.jpg"/>
+            </a>
+            <div className="mt-4">
+              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+              <h2 className="text-gray-900 title-font text-lg font-medium">Hoodies</h2>
+              <p className="mt-1">₹499</p>
+            </div>
+          </div></Link>
+        </div>
+      </div>
+    </section>
   )
 }
 
