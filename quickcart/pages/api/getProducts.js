64e28b1b7e408efdf2304b163e8dcd10c41ed7fc
pkcoder471 <pklid471@gmail.com>
2024-02-26 01:41:41 +0530

    displaying tshirts through db

--- quickcart/pages/api/getProducts.js
+++ quickcart/pages/api/getProducts.js
@@ -2,8 +2,9 @@ import Product from '@/models/Product';
 import connectToDb from '@/middleware/mongoose';
 
 const handler = async (req,res) =>{
+
     try {
-        let products = await Product.find();
+        let products = await Product.find({"category":req.query.category});
         return res.status(200).json({products});
     } catch (err) {
         console.log(err);
--- quickcart/pages/tshirts.js
+++ quickcart/pages/tshirts.js
@@ -1,96 +1,38 @@
 import React from 'react'
 import Link from 'next/link'
-const tshirts = () => {
+
+const tshirts = ({products}) => {
+  
   return (
     <section className="text-gray-600 body-font">
       <div className="container px-16 py-16 mx-auto">
         <div className="flex flex-wrap -m-4">
-          
-            <Link href={'/product/hoodie'} className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-sm cursor-pointer "><div>
+          {products.map((item)=>{
+            return <Link href={'/product/hoodie'} className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-sm cursor-pointer "><div>
             <a className="  relative h-80 m-auto rounded overflow-hidden">
-              <img alt="ecommerce"  className="h-80 m-auto block" src="https://m.media-amazon.com/images/I/61b4R3TMXoL._SY879_.jpg"/>
+              <img alt="ecommerce"  className="h-80 m-auto block" src={item.img}/>
             </a>
             <div className="mt-4">
               <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-              <h2 className="text-gray-900 title-font text-lg font-medium">Tshirts</h2>
-              <p className="mt-1">₹499</p>
+              <h2 className="text-gray-900 title-font text-lg font-medium">{item.category}</h2>
+              <p className="mt-1">{item.price}</p>
             </div>
-          </div></Link>
-          
-          <Link href={'/product/hoodie'} className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-sm cursor-pointer "><div>
-            <a className="  relative h-80 m-auto rounded overflow-hidden">
-              <img alt="ecommerce"  className="h-80 m-auto block  " src="https://m.media-amazon.com/images/I/61b4R3TMXoL._SY879_.jpg"/>
-            </a>
-            <div className="mt-4">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-              <h2 className="text-gray-900 title-font text-lg font-medium">Tshirts</h2>
-              <p className="mt-1">₹499</p>
-            </div>
-          </div></Link>
-          <Link href={'/product/hoodie'} className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-sm cursor-pointer "><div>
-            <a className="  relative h-80 m-auto rounded overflow-hidden">
-              <img alt="ecommerce"  className="h-80 m-auto block  " src="https://m.media-amazon.com/images/I/61b4R3TMXoL._SY879_.jpg"/>
-            </a>
-            <div className="mt-4">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-              <h2 className="text-gray-900 title-font text-lg font-medium">Tshirts</h2>
-              <p className="mt-1">₹499</p>
-            </div>
-          </div></Link>
-          <Link href={'/product/hoodie'} className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-sm cursor-pointer "><div>
-            <a className="  relative h-80 rounded overflow-hidden">
-              <img alt="ecommerce"  className="h-80 m-auto block  " src="https://m.media-amazon.com/images/I/61b4R3TMXoL._SY879_.jpg"/>
-            </a>
-            <div className="mt-4">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-              <h2 className="text-gray-900 title-font text-lg font-medium">Tshirts</h2>
-              <p className="mt-1">₹499</p>
-            </div>
-          </div></Link>
-          <Link href={'/product/hoodie'} className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-sm cursor-pointer "><div>
-            <a className="  relative h-80 m-auto rounded overflow-hidden">
-              <img alt="ecommerce"  className="h-80 m-auto block  " src="https://m.media-amazon.com/images/I/61b4R3TMXoL._SY879_.jpg"/>
-            </a>
-            <div className="mt-4">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-              <h2 className="text-gray-900 title-font text-lg font-medium">Tshirts</h2>
-              <p className="mt-1">₹499</p>
-            </div>
-          </div></Link>
-          <Link href={'/product/hoodie'} className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-sm cursor-pointer "><div>
-            <a className="  relative h-80 m-auto rounded overflow-hidden">
-              <img alt="ecommerce"  className="h-80 m-auto block  " src="https://m.media-amazon.com/images/I/61b4R3TMXoL._SY879_.jpg"/>
-            </a>
-            <div className="mt-4">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-              <h2 className="text-gray-900 title-font text-lg font-medium">Tshirts</h2>
-              <p className="mt-1">₹499</p>
-            </div>
-          </div></Link>
-          <Link href={'/product/hoodie'} className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-sm cursor-pointer "><div>
-            <a className="  relative h-80 m-auto rounded overflow-hidden">
-              <img alt="ecommerce"  className="h-80 m-auto block  " src="https://m.media-amazon.com/images/I/61b4R3TMXoL._SY879_.jpg"/>
-            </a>
-            <div className="mt-4">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-              <h2 className="text-gray-900 title-font text-lg font-medium">Tshirts</h2>
-              <p className="mt-1">₹499</p>
-            </div>
-          </div></Link>
-          <Link href={'/product/hoodie'} className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-sm cursor-pointer "><div>
-            <a className="  relative h-80 m-auto rounded overflow-hidden">
-              <img alt="ecommerce"  className="h-80 m-auto block  " src="https://m.media-amazon.com/images/I/61b4R3TMXoL._SY879_.jpg"/>
-            </a>
-            <div className="mt-4">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-              <h2 className="text-gray-900 title-font text-lg font-medium">Tshirts</h2>
-              <p className="mt-1">₹499</p>
-            </div>
-          </div></Link>
+          </div></Link>})}
         </div>
       </div>
     </section>
   )
+}
+
+export async function getServerSideProps() { 
+  const response = await fetch("http://localhost:3000/api/getProducts?category=Tshirts", {
+    method: "GET",
+  });
+  const json = await response.json()
+  let Products = JSON.parse(JSON.stringify(json));
+  return { 
+      props: { products: Products.products }, 
+  }; 
 }
 
 export default tshirts
